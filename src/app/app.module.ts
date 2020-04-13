bc7fb17c8add679550d8e961e39122c0bc48b5cc
Judith <mjudithmft@gmail.com>
2020-04-13 21:16:36 +0200

    feat: AlmeríaJS 2.0 :rocket:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+import { MatStepperModule } from '@angular/material/stepper';
 
 @NgModule({
   declarations: [AppComponent, FooterComponent, HomeComponent],
@@ -30,6 +31,7 @@ import { AppRoutingModule } from './app-routing.module';
     MatButtonModule,
     MatRippleModule,
     MatRadioModule,
+    MatStepperModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,19 +1,38 @@
 import { Component } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: { displayDefaultIndicatorType: false },
+    },
+  ],
 })
 export class HomeComponent {
+  talks = [
+    {
+      date: '16 abril',
+      image: '/assets/images/talks/pinchito.png',
+      link: 'https://www.meetup.com/es-ES/almeriajs/events/269973701/',
+    },
+    { date: '23 abril', image: '/assets/images/talks/pablo.png' },
+    { date: '30 abril', image: '/assets/images/talks/konstantin.png' },
+    { date: '6 mayo', image: '/assets/images/talks/ari.png' },
+  ];
+
   constructor(title: Title, meta: Meta) {
     title.setTitle('AlmeríaJS');
     meta.updateTag({
       name: 'description',
       content:
         'Somos un grupo de apasionados de la tecnología que nos juntamos cada vez que ' +
-        'tenemos ocasión para hablar de cualquier tema relacionado con Javascript.'
+        'tenemos ocasión para hablar de cualquier tema relacionado con Javascript.',
     });
   }
 }
